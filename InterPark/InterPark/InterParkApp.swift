21b9995dd873946d65b1fcb62a3aae0e2af7415c
김주영 <joo-young.kim@navercorp.com>
2022-08-05 15:52:16 +0900

    초기 프로젝트 세팅

---
+++ InterPark/InterPark/InterParkApp.swift
@@ -0,0 +1,17 @@
+//
+//  InterParkApp.swift
+//  InterPark
+//
+//  Created by Joo Young Kim on 2022/08/05.
+//
+
+import SwiftUI
+
+@main
+struct InterParkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
